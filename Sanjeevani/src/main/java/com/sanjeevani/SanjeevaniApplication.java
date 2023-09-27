27cce97acaa2adad720d59dcf996f3dca52d73fa
sagar <sagarbhadrawle@gmail.com>
2023-09-27 13:22:38 +0530

    create project format

---
+++ Sanjeevani/src/main/java/com/sanjeevani/SanjeevaniApplication.java
@@ -0,0 +1,13 @@
+package com.sanjeevani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanjeevaniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanjeevaniApplication.class, args);
+	}
+
+}
---
+++ Sanjeevani/src/test/java/com/sanjeevani/SanjeevaniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanjeevani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanjeevaniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
